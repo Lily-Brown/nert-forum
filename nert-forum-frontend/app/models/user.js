59c25cf6adbf134528aad7182ff0c190be6b3946
LilyCole <Lilia.Cole@gmail.com>
2016-11-21 21:54:33 -0800

    starts over

--- nert-forum-frontend/app/models/user.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  name: DS.attr('string'),
-  email: DS.attr('string'),
-  neighborhood: DS.attr('string'),
-  image: DS.attr('string'),
-  password_digest: DS.attr('string')
-});
