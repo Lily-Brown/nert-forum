f0976b32db2e5c3ecf0b0dbdc6fc3c46d5ec44b0
LilyCole <Lilia.Cole@gmail.com>
2016-11-21 20:04:18 -0800

    adds user controller methods and tests

--- nert-forum-backend/spec/requests/api/users_spec.rb
+++ nert-forum-backend/spec/requests/api/users_spec.rb
@@ -6,13 +6,13 @@ describe "User API Tests" do
     @user = User.create(email: "test@test.com", name: "Test", password: "password")
   end
 
-  it "GET /users should return 200" do
+  it "user#index: GET /users should return 200" do
     get "/users"
 
     expect(response).to have_http_status(200)
   end
 
-  it "POST /users should return 201" do 
+  it "user#create: POST /users should return 201" do 
     post "/users", params: {
       user: {
         email: "test@test.com",
@@ -24,7 +24,7 @@ describe "User API Tests" do
     expect(response).to have_http_status(201)
   end
 
-  it "POST /users should return 400 for bad request" do
+  it "user#create: POST /users should return 400 for bad request" do
     post "/users", params: {
       user: {
         email: "test@test.com",
@@ -35,40 +35,30 @@ describe "User API Tests" do
     expect(response).to have_http_status(400)
   end
 
-  context 'when user is logged in && current_user' do # TODO: Fix 302 errors
+  it "user#show: GET /users/:id should return 200" do
+    get "/users/#{@user.id}"
 
-    before(:each) do # TODO: fix validation for this test, ONLY WORKS WHEN BEFORE_ACTION COMMENTED OUT
-        current_user = @user
-    end
-
-    it "GET /users/:id/edit should return 200" do
-      
-      get "/users/#{@user.id}/edit"
-
-      expect(response).to have_http_status(200)
-    end
-
-    it "PUT /users/:id/edit should return 204" do
-
-      put "/users/#{@user.id}", params: {
-        user: {
-          email: "test@test.com",
-          name: "Test",
-          password: "password"
-        }
-      }
-
-      expect(response).to have_http_status(204)
-    end
-
-    it "DELETE /users/:id should return 200" do
-
-      delete "/users/#{@user.id}"
-
-      expect(response).to have_http_status(200)
-    end
-
+    expect(response).to have_http_status(200)
   end
 
+  it "user#update: PUT /users/:id/ should return 204" do
+
+    put "/users/#{@user.id}", params: {
+      user: {
+        email: "test@test.com",
+        name: "Test",
+        password: "password"
+      }
+    }
+
+    expect(response).to have_http_status(204)
+  end
+
+  it "user#destroy: DELETE /users/:id should return 200" do
+
+    delete "/users/#{@user.id}"
+
+    expect(response).to have_http_status(200)
+  end
   
 end
