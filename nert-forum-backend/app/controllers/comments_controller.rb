3290ed32885c8b83ebfecc0d7d14e87aacf8065c
LilyCole <Lilia.Cole@gmail.com>
2016-11-22 11:53:36 -0800

    adds comment controller methods and tests

--- nert-forum-backend/app/controllers/comments_controller.rb
+++ nert-forum-backend/app/controllers/comments_controller.rb
@@ -1,2 +1,54 @@
 class CommentsController < ApplicationController
+
+  def index
+    render :json => Comment.all, status: 200
+  end
+
+  def create
+    comment = Comment.create(comment_params)
+
+    if comment.valid?
+      render :json => comment, status: 201
+    else
+      render :json => {error: "Comment validation failed"}, status: 400
+    end
+  end
+
+  def show
+    comment = Comment.find(params[:id])
+
+    render :json => comment, status: 200
+  end
+
+  def edit
+    comment = Comment.find(params[:id])
+
+    render :json => comment, status: 200
+  end
+
+  def update
+    comment = Comment.find(params[:id])
+
+    if comment.update_attributes(comment_params)
+      render :json => {success: "Comment updated successfully"}, status: 204
+    else
+      render :json => {error: "Failed to update Comment"}, status: 400
+    end
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    if comment.destroy
+      render :json => {success: "Comment deleted successfully"}, status: 204
+    else
+      render :json => {error: "Comment deletion failed"}, status: 400
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:data).require(:attributes).permit(:text_body, :user_id, :post_id)
+  end
+
 end
