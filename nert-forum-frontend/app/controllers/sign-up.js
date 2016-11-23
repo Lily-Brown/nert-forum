941f7f2c2cd5baa342842f2dec28861015179f04
LilyCole <Lilia.Cole@gmail.com>
2016-11-23 12:03:19 -0800

    adds sign-up flow and adds new user

--- nert-forum-frontend/app/controllers/sign-up.js
+++ nert-forum-frontend/app/controllers/sign-up.js
@@ -3,16 +3,11 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     addUser() { 
-      if (this.get(model.password) == this.get(model.passwordConfirmation)) {
-        this.get('store').createRecord('user', {
-            name: this.get('model.name'),
-            email: this.get('model.email'),
-            password: this.get('password')
-          }).save();
+      if (this.get('model.password') === this.get('passwordConfirmation')) {
+        this.get('model').save();
       } else {
         alert('Passwords need to match.');
       }
-
     }
   }
 });
